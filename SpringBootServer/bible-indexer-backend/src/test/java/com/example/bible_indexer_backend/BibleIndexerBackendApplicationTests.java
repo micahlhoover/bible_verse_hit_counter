fe17b3d0649615630be6d3b46667ed81454362e0
Micah Hoover <m.l.hoover@avanade.com>
2026-06-26 15:13:49 -0400

    Adding Spring project to host backend indexed meta

---
+++ SpringBootServer/bible-indexer-backend/src/test/java/com/example/bible_indexer_backend/BibleIndexerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bible_indexer_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibleIndexerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
