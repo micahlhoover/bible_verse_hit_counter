9ab1dd49516d194f1ef3361c85656a0599c7eaf6
Micah Hoover <m.l.hoover@avanade.com>
2026-07-06 14:41:08 -0400

    CORS allowed for both deployed and local

--- SpringBootServer/bible-indexer-backend/src/main/java/com/example/bible_indexer_backend/MetaController.java
+++ SpringBootServer/bible-indexer-backend/src/main/java/com/example/bible_indexer_backend/MetaController.java
@@ -10,7 +10,10 @@ import java.nio.charset.StandardCharsets;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
-@CrossOrigin(origins = "https://underratedbibleverses.netlify.app")
+@CrossOrigin(origins = {
+        "https://underratedbibleverses.netlify.app",
+        "http://localhost:4200"
+})
 public class MetaController {
 
     @GetMapping("/books")
